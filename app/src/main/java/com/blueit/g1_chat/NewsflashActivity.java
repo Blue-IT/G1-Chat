c81f7b90aaa094c0a5491685349bdefc28da38bf
Christoffer Lindell <christoffer@lindell.io>
2015-09-22 17:12:45 +0200

    Add insert newsflash functionality.

--- app/src/main/java/com/blueit/g1_chat/NewsflashActivity.java
+++ app/src/main/java/com/blueit/g1_chat/NewsflashActivity.java
@@ -2,29 +2,50 @@ package com.blueit.g1_chat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class NewsflashActivity extends AppCompatActivity {
+public class NewsflashActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText newsflashText;
+
+    private ArrayList<String> TEMP_newsflashTable;
+    private ArrayAdapter<String> TEMP_newsflashTableAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set view
         setContentView(R.layout.activity_newsflash);
 
-        ArrayList<String> myStringArray = new ArrayList<String>();
-        myStringArray.add("Number one");
-        myStringArray.add("Number two");
-        myStringArray.add("Number three");
+        // Initialize text field
+        newsflashText = (EditText) findViewById(R.id.newsflash_text);
+        newsflashText.setInputType(InputType.TYPE_CLASS_TEXT
+                       | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, myStringArray);
+        // Initialize placeholder database
+        TEMP_newsflashTable = new ArrayList<String>();
+        TEMP_newsflashTable.add("Lorem");
+        TEMP_newsflashTable.add("Ipsum");
+
+        // Configure the adapter which feeds database info into the list
+        TEMP_newsflashTableAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, TEMP_newsflashTable);
         ListView listView = (ListView) findViewById(R.id.newsflash_list);
-        listView.setAdapter(adapter);
+        listView.setAdapter(TEMP_newsflashTableAdapter);
+
+        // Setup submit button
+        setClick(R.id.newsflash_submit);
+
     }
 
     @Override
@@ -47,5 +68,52 @@ public class NewsflashActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View v)
+    {
+        int id = v.getId();
+
+        if (id == R.id.newsflash_submit)
+        {
+            createNewsflash();
+        }
+
+    }
+
+    public void createNewsflash()
+    {
+        // Get input
+        String input = newsflashText.getText().toString();
+
+        // Validate
+        Log.d("G1CHAT", "CreateNewsflash");
+        Log.d("G1CHAT", input);
+        if (input == "") {
+            Log.d("G1CHAT", "Early exit");
+            return;
+        }
+
+        // Create and insert
+        TEMP_newsflashTable.add(input);
+        TEMP_newsflashTableAdapter.notifyDataSetChanged();
+        Log.d("G1CHAT", "Newsflash created!");
+        // Reset input
+        newsflashText.setText("");
+    }
+
+    /**
+     * Sets the click listener for a view with given id.
+     *
+     * @param id
+     *            the id
+     * @return the view on which listener is applied
+     */
+    public View setClick(int id)
+    {
+        View v = findViewById(id);
+        if (v != null)
+            v.setOnClickListener(this);
+        return v;
     }
 }
