754565d2495c2ac4041340950b07c0552a2348dd
Cohdex <jonathan.cohlin@gmail.com>
2015-09-22 11:15:39 +0200

    Added comment

--- app/src/main/java/com/blueit/g1_chat/MainActivity.java
+++ app/src/main/java/com/blueit/g1_chat/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set main layout
         setContentView(R.layout.activity_main);
     }
 
